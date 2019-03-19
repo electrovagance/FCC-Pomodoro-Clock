cd79aacbb7a6d5842133a9b6ebb7ce796735f46b
Malak Tayeh <malaktayeh22@gmail.com>
2019-03-18 21:46:52 -0400

    added logic for starting, pausing, stopping, and resetting counter

--- src/App.js
+++ src/App.js
@@ -10,19 +10,50 @@ class App extends Component {
             seconds: 0,
             breakLength: 5,
             sessionLength: 25,
-            stopCountDown: false
+            stopCountDown: true
+        }
+      this.startOrStopCountDown = this.startOrStopCountDown.bind(this);
+      this.resetCountDown = this.resetCountDown.bind(this);
+
+    }
+
+
+    updateCountDown = () => {
+
+    }
+
+    startOrStopCountDown = () => {
+        if (this.state.stopCountDown) {
+            // setInterval calls countdown function every second
+            this.setState({ stopCountDown: false });
+            this.timer = setInterval(
+                () => this.startCountdown(),
+                1000
+            );
+        } else {
+            this.setState({ stopCountDown: true });
+            this.stopCountDown();
         }
     }
 
-    componentDidMount() {
-        // setInterval calls countdown function every second
-        this.timer = setInterval(
-            () => this.countdown(),
-            1000
-        );
+    resetCountDown = () => {
+        clearInterval(this.timer);
+        this.setState({ 
+            totalSeconds: 1500, // total seconds (25 minutes * 60 seconds)
+            minutes: 25,
+            seconds: 0,
+            breakLength: 5,
+            sessionLength: 25,
+            stopCountDown: true
+        })
     }
 
-    countdown = () => {
+    stopCountDown = () => {
+        clearInterval(this.timer);
+        this.setState({ stopCountDown: true })
+    }
+
+    startCountdown = () => {
         // save state in local variables
         let tempMin = this.state.minutes,
             tempSec = this.state.seconds,
@@ -42,8 +73,7 @@ class App extends Component {
 
         // stops countdown if seconds and minutes are down to zero, stops countdown and updates state in app
         if (this.state.minutes === 0 && this.state.seconds === 0) {
-            clearInterval(this.timer);
-            this.setState({ stopCountDown: true })
+            this.resetCountDown();
         }
 
     }
@@ -52,21 +82,23 @@ class App extends Component {
         return (
             <div id="pomodoro-app">
                 <h1>Pomodoro Clock</h1>
-                <p id="timer-label">{this.state.stopCountDown === false ? 'Counting down...' : 'Stopped'}</p>
+                <p id="timer-label">{this.state.stopCountDown === false ? 'Counting down...' : 'Session inactive'}</p>
                 <p id="time-left">{this.state.minutes < 10 ? "0" + this.state.minutes : this.state.minutes}:{this.state.seconds < 10 ? "0" + this.state.seconds : this.state.seconds}</p>
                 <div>
-                    <button id="start_stop">{this.state.stopCountDown === true ? '▻' : '❚❚'}</button>
-                    <button id="reset">↺</button>
+                    <button id="start_stop" onClick={this.startOrStopCountDown}>
+                        {this.state.stopCountDown === true ? '▻' : '❚❚'}
+                    </button>
+                    <button id="reset" onClick={this.resetCountDown}>↺</button>
                 </div>
                 <div>
-                    <h3 id="break-label">Break Length: <span id="break-length">{this.state.breakLength}</span></h3>
-                    <button id="break-decrement">↑</button>
-                    <button id="break-increment">↓</button>
+                    <h3 id="break-label">Break Length: <span id="break-length">{this.state.breakLength}</span> min.</h3>
+                    <button id="break-increment" onClick={this.incrementBreakLength}>↑</button>
+                    <button id="break-decrement" onClick={this.decrementBreakLength}>↓</button>
                 </div>
                 <div>
-                    <h3 id="session-label">Session Length: <span id="session-length">{this.state.sessionLength}</span></h3>
-                    <button id="session-decrement">↑</button>
-                    <button id="session-increment">↓</button>
+                    <h3 id="session-label">Session Length: <span id="session-length">{this.state.sessionLength}</span> min.</h3>
+                    <button id="session-increment" onClick={this.incrementSessionLength}>↑</button>
+                    <button id="session-decrement" onClick={this.decrementSessionLength}>↓</button>
                 </div>
             </div>
         )
